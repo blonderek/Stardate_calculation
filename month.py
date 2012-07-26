20c3fb1709d0974716e045f7424392ab8f53c182
DOlsen <DOlsen@email.com>
2012-07-25 19:42:22 -0700

    New version
    I realize that this  could be done much simpler with a dictionary keying
    each month to its number of days as a value, but this was kind of an
    exercise for me in learning python.

--- month.py
+++ month.py
@@ -1,6 +1,7 @@
 import math
+from sys import exit
 
-months = {1:'January', 2:'February', 3:'March', 4:'April', 5:'May', 6:'June', 7:'July', 8:'August', 9:'September', 10:'October', 11:'November', 12:'December'}
+months = {'February':2, 'October':10, 'March':3, 'August':8, 'May':5, 'January':1, 'June':6, 'September':9, 'April':4, 'December':12, 'July':7, 'November':11}
 
 def calc(m):
     hi = int((153*((m+1)+12*(int((14-(m+1))/12))-3)+2)/5)-int((14-(m+1))/12)*365
@@ -9,21 +10,16 @@ def calc(m):
 
 def days(month=''):
     if month == '':
-        month = raw_input("Which month? Type 'all' to see a list. ")
+        month = raw_input('Which month? ')
+    if month == 'e':
+        exit()
     month = month.capitalize()
-    if month == 'All':
-        for k, n in months.iteritems():
-            d = calc(k)
-            print "{0} has {1} days".format(n, d)
-   
-    elif month in months.values():
-        for k, n in months.iteritems():
-            if n == month:
-                d = calc(k)
-                print "{0} has {1} days".format(n, d)
-                break
-    else:
+    try:
+        m = months[month]
+    except KeyError:
         month = raw_input('Invalid month. Try again. ')
         days(month)
-
+    else:
+        d = calc(m)
+        print "{0} has {1} days".format(month, d)
 days()
